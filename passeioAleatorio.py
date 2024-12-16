5bcde1b0922eea7159b9ad8d27951fbff760d32d
Milton <miltonbarroca@gmail.com>
2024-12-16 06:21:45 -0300

    dia 4

---
+++ passeioAleatorio.py
@@ -0,0 +1,18 @@
+import random
+import matplotlib.pyplot as plt  # Corrigido: importe o pyplot
+
+# Caminhada aleatória
+position = 0
+walk = [position]
+nsteps = 1000
+for _ in range(nsteps):
+    step = 1 if random.randint(0, 1) else -1
+    position += step
+    walk.append(position)
+
+# Plotando os 100 primeiros passos
+plt.plot(walk[:100])
+plt.title("Caminhada Aleatória")
+plt.xlabel("Passos")
+plt.ylabel("Posição")
+plt.show()
